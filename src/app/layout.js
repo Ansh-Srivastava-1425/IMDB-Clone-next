65451d77ac10a3fa91a450f8a9f0cefff45266ad
Ansh-Srivastava-1425 <kumar.ansh7591@gmail.com>
2026-05-26 10:21:50 +0530

    About Page

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Provider from "./Provider";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,18 +22,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-  lang="en"
-  className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-  suppressHydrationWarning
->
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
+    >
       <body className="min-h-full flex flex-col">
-        <Provider >
-
-
-        <Header />
-        {children}
+        <Provider>
+          <Header />
+          <Navbar />
+          {children}
         </Provider>
-        </body>
+      </body>
     </html>
   );
 }
